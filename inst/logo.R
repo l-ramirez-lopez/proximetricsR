07f557cec67cf12abaa5fb6869c732c2850360b4
l-ramirez-lopez <leonardo.ramirez-lopez23@imperial.ac.uk>
2026-07-31 20:45:33 +0200

    new logo

--- inst/logo.R
+++ inst/logo.R
@@ -6,26 +6,11 @@ library(ggtext)
 library(systemfonts)
 library(prospectr)
 
-#   Data  
-data(NIRsoil)
-mspc <- savitzkyGolay(NIRsoil$spc, m = 2, p = 2, w = 91)
+library(magick)
 
-nspc <- 350
-set.seed(123)
-some_spc <- mspc[sample(nrow(mspc), nspc), ]
-
-wavs <- as.numeric(colnames(mspc))
-df <- data.frame(
-  wavelength = rep(wavs, times = nspc),
-  absorbance = as.vector(t(some_spc)),
-  sample = rep(paste0("S", 1:nspc), each = length(wavs))
-)
-
-#   Baseline ring  
-baseline_df <- data.frame(
-  wavelength = seq(min(wavs), max(wavs), length.out = 200),
-  absorbance = 0
-)
+img <- image_read("inst/zebra_handdrawn-wb.png")
+print(img)          # displays it
+image_info(img)     # check it has alpha channel (colorspace/format info)
 
 #   Fonts  
 font_add(
@@ -56,79 +41,53 @@ palette <- c(
   accent3 = "#EAC473"
 )
 
-#   Polar plot: baseline ring + spectra  
-p <- ggplot() +
-  geom_path(
-    data = baseline_df,
-    aes(x = wavelength, y = absorbance),
-    linewidth = 0.5,
-    colour = "white",
-    alpha = 0.6
-  ) +
-  geom_line(
-    data = df,
-    aes(x = wavelength, y = absorbance, group = sample),
-    linewidth = 0.6,
-    colour = "white",
-    alpha = 0.015
-  ) +
-  coord_polar(theta = "x") +
-  theme_void() +
-  theme(legend.position = "none")
 
 #   Sticker — spotlight OFF, will be added manually as centered subview  
 s <- sticker(
-  p,
+  img,
   package = "",
   s_x = 1,
-  s_y = 1,
-  s_width = 2.5,
-  s_height = 2.5,
-  h_fill = palette["secondary3"],
+  s_y = 1.05,
+  s_width = 1.4,
+  s_height = 1.4,
+  h_fill = "#4f719a",
   h_size = 1.4,
   h_color = "#3B82F6", ##64B445
   filename = "proximetricsr_hex.png",
   dpi = 300,
-  url = "https://github.com/buchi-labortechnik-ag/proximetricsR",
-  u_x = 0.94,
-  u_y = 0.085,
+  url = "https://buchi-nir.io/",
+  # u_x = 0.94,
+  # u_y = 0.085,
   u_family = "helvetica_neue_ltex",
   u_color = "white",
-  u_size = 2.6,
+  u_size = 3,
   spotlight = FALSE
 )
 
-#   Centered hexbin spotlight using hexSticker's internal function  
-
-hexSticker_spotlight <- function(alpha) {
-  vals_x <- rnorm(5e+05, sd = 2, mean = 0)
-  vals_y <- rnorm(5e+05, sd = 2, mean = 0)
-  hexbin::hexbinplot(vals_x ~ vals_y, colramp = hexSticker:::whiteTrans(alpha), 
-             colorkey = FALSE, bty = "n", scales = list(draw = FALSE), 
-             xlab = "", ylab = "", border = NA, par.settings = list(axis.line = list(col = NA)))
-}
-
-s <- s +
-  geom_subview(
-    subview = hexSticker_spotlight(alpha = 0.4),
-    x = 1, y = 1,
-    width = 3, height = 3
-  )
-
-#   Wordmark on top  
 s <- s +
   geom_richtext(
-    aes(x = 1, y = 1),
-    label = "<span style='color:#FFFFFF;'>proximetrics</span><span style='color:#64B445;'>R</span>",
+    aes(x = 1, y = 0.50),
+    label = "<span style='color:#FFFFFF;'>proximetrics</span><span style='color:#FFFFFF00;'>R</span>",
     family = "helvetica_neue_bdex",
     fontface = "plain",
-    size = 13,
+    size = 12,
     fill = NA,
     label.color = NA
-  )
+  ) +
+  geom_richtext(
+    aes(x = 1.02, y = 0.42),
+    label = "<span style='color:#FFFFFF00;'>proximetrics</span><span style='color:#64B445;'>R</span>",
+    family = "helvetica_neue_bdex",
+    fontface = "plain",
+    size = 12,
+    fill = NA,
+    # hjust = 0,
+    angle = 8,
+    label.color = NA
+  ) 
 
 s
 
 ggsave("man/figures/logo.png", s, width = 43.94, height = 50.8, units = "mm", dpi = 300, bg = "transparent")
 
-# s
+
